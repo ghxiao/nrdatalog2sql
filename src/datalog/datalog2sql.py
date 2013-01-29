5de412cabc30dee2d513d4ce0567f2d49720c725
Guohui Xiao <gh.xiao@gmail.com>
2013-01-30 00:19:09 +0100

    first version to convert nr-datalog to sql

--- src/datalog/datalog2sql.py
+++ src/datalog/datalog2sql.py
@@ -1,48 +1,150 @@
-#!/usr/local/bin/python2.7
+#!/usr/bin/env python
 # encoding: utf-8
 '''
 datalog.datalog2sql -- convert datalog to SQL
 
-datalog.datalog2sql is a description
-
-It defines classes_and_methods
-
-@author:     xiao
-        
-@copyright:  2013 TUW. All rights reserved.
-        
-@license:    BSD
-
-@contact:    xiao@kr.tuwien.ac.at
-@deffield    updated: Updated
+Usage: ./datalog2sql.py datalog.dl tbox_name.txt> query.sql
 '''
 
 import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import pygraphviz
+from collections import defaultdict, Counter
+import itertools
+
+from datalog.model import Variable
 import datalog.yacc
 
-def main():
+def main(argv):
     parser = datalog.yacc.parser
-    datalogString = open('long.prestorew.dl').read()
+    datalogString = open(argv[1]).read()
     program = parser.parse(datalogString)
     G = nx.DiGraph()
     for rule in program.rules:
         for body_literal in rule.body:
-            G.add_edge(body_literal.classicLiteral.atom.predicate, rule.head.predicate)
-    
-#    nx.draw(G)
-    #nx.draw_spectral(G)
-    nx.draw_graphviz(G)
+            G.add_edge(body_literal.predicate, rule.head.predicate)
 
-    depends =  nx.topological_sort(G)
+    # vocab is a map predicate_name -> (encode, arity)
+    vocab =  read_tbox_name(argv[2])
+    
+    # print vocab
 
-    print depends
+    depends = nx.topological_sort(G)
     
+    sql = "WITH \n" + ",\n".join([ edb2sfw(p, *vocab.get(p)) 
+                               for p in (set(depends) & set(vocab.keys()))])
+
+    sql += ",\n"
+
+    ctes = []
     
-    #print program
+    for p in depends:
+        
+        rules = [r for r in program.rules if r.head.predicate == p]
+
+        if len(rules) == 0:
+            continue
+
+        cte = rules2cte(rules)
+        ctes.append(cte)
+
+    sql += ",\n".join(ctes)
+
+    sql += "SELECT COUNT(*) FROM %s" % depends[-1]
+    
+#        print cte
+    print sql
+
+  
+def rules2cte(rules) :
+    '''convert rules with a common head predicate to a cte'''
+    #"%s AS (SELECT ... UNION SELECT ... UNION ... )"
+    return "%s AS (%s)" % (rules[0].head.predicate, 
+                           ( " UNION\n ").join(rule2sfw(rule) for rule in rules))
+
+        
+def rule2sfw(rule) :
+    ''' convert a rule to a SFW block '''
+    # SELECT att_0, att2 FROM ... WHERE ...
+    var2LiteralsMap = defaultdict(list)
+    # { var : (leteral, term_index, literal_index) }
+    for k,lit in enumerate(rule.body):
+        for (i,t) in enumerate(lit.terms):
+            var2LiteralsMap[t].append((lit,i,k))
+
+    vars = [ 'atom_%d.att_%d ' % (rule.body.index(lit_idx[0]), lit_idx[1])
+      for (lit_idx, var) in ((var2LiteralsMap[term][0], term) 
+                             for term in rule.head.terms #if isinstance(term, Variable)
+            )]
+
+    vars = [ '%s AS att_%d' % (v,i) for i,v in enumerate(vars)]
+
+    s = "\n SELECT %s " % ",".join(vars)
+
+    f = "\n FROM\n%s" % ",\n".join(
+        ["%s AS atom_%d" % (literal.predicate, index) 
+         for index, literal in enumerate(rule.body)])
+
+    w = ""
+
+    conds = list(itertools.chain (*[["atom_%d.att_%d = atom_%d.att_%d" % 
+                                     (lit_idx_lst[0][2], lit_idx_lst[0][1], 
+                                      lit_idx_lst[k][2], lit_idx_lst[k][1]) 
+                                for k in xrange(1, len(lit_idx_lst))]
+                               for lit_idx_lst in var2LiteralsMap.values()]))
+    
+    if len(conds):
+        w = "\n WHERE " + " AND ".join(conds)
+
+    sfw = "(%s %s %s)" % (s, f, w)
+    return sfw
+
+def var_lits(var, lits, var2LiteralsMap):
+    pass
+
+# type_ defined in owlgres
+# - 1 : concept
+# - 2 : object property
+# - 3 : data property
+# - 4 : annotation type
+def edb2sfw(predicate, encode, type_):
+    if type_ == 1:
+        return ('%s AS ( \n'
+                '  SELECT ca.individual AS att_0 \n'
+                '  FROM concept_assertion AS ca \n' 
+                '  WHERE concept = %d \n'
+                ')') % (predicate, encode)
+    elif type_ == 2:
+        return ('%s AS ( \n' 
+                '  SELECT ora.a AS att_0, ora.b AS att_1 \n' 
+                '  FROM object_role_assertion AS ora \n' 
+                '  WHERE object_role = %d \n' 
+                ')') % (predicate, encode)
+    elif type_ == 3:
+        return ('%s AS ( \n' 
+                '  SELECT dra.a AS att_0, dra.b AS att_1 \n'
+                '  FROM data_role_assertion AS dra \n'
+                '  WHERE data_role = %d \n' 
+                ')') % (predicate, encode)
+    else:
+        raise "Unknown suppored type %d" % type_
+
+def read_tbox_name(fname):
+    with open(fname) as f:
+        f.next()
+        f.next()
+        # { prdicate : (id, type) }
+        return { frag(sp[4].strip()) : (int(sp[0].strip()), int(sp[1].strip()))  
+                 for sp in (line.strip().split('|')  
+                            for line in f) if len(sp)==5 if '#' in sp[4]}
+
+def frag(url):
+    return url[url.rindex('#')+1:]
+
     
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
--- src/datalog/model.py
+++ src/datalog/model.py
@@ -4,8 +4,21 @@ Created on Jan 27, 2013
 @author: xiao
 '''
 
+#class CommonEqualityMixin(object):
+#
+#    def __eq__(self, other):
+#        return (isinstance(other, self.__class__)
+#            and self.__dict__ == other.__dict__)
+#
+#    def __ne__(self, other):
+#        return not self.__eq__(other)
+
 class Term(object):
-    pass
+    def __eq__(self, other):
+        return self.name == other.name
+    
+    def __hash__(self):
+        return hash(self.name)
 
 class Variable(Term):
     def __init__(self, name):
@@ -13,6 +26,9 @@ class Variable(Term):
         
     def __str__(self):
         return self.name
+    
+    def __repr__(self):
+        return "datalog.model.Variable('%s')" % self.name
         
 class Constant(Term):
     def __init__(self, name):
@@ -20,6 +36,9 @@ class Constant(Term):
         
     def __str__(self):
         return self.name
+    
+    def __repr__(self):
+        return "datalog.model.Constant('%s')" % self.name
 
 class NafLiteral(object):
     def __init__(self, predicate, terms, neg=False, naf=False):
@@ -33,7 +52,9 @@ class NafLiteral(object):
             "not " if self.naf else "", 
             "-" if self.neg else "", 
             self.predicate, ",".join(str(x) for x in self.terms))
-        
+
+    def __repr__(self):
+        return "datalog.model.NafLiteral('%s', '%s', '%s', '%s')" % (self.predicate, self.terms, self.neg, self.naf)
 
 class Rule(object):
     def __init__(self, head, body):
@@ -49,3 +70,12 @@ class Program(object):
 
     def __str__(self):
         return "\n".join(str(x) for x in self.rules)
+    
+if __name__ == '__main__':
+    x1 = Variable('X')
+    x2 = Variable('X')
+    from collections import defaultdict
+    d = defaultdict(list)
+    d[x1].append(1)
+    d[x2].append(2)
+    print d
